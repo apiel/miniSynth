da6286c1c4f637404c6b70d71af8724060bb606d
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-03-12 22:06:55 +0100

    looooooooop

--- instrument/io_audio_loop.h
+++ instrument/io_audio_loop.h
@@ -3,10 +3,14 @@
 
 #include <Arduino.h>
 
-template<class AudioCore = void>
-class IO_AudioLoop {
-   private:
-    AudioCore* core;
+#include "../Pattern.h"
+#include "../io_arp_patterns.h"
+
+template <class AudioCore = void>
+class IO_AudioLoop
+{
+private:
+    AudioCore *core;
 
     // byte currentStep = 0;
     // Step lastStep;
@@ -18,12 +22,27 @@ class IO_AudioLoop {
     //     }
     // }
 
-   public:
+public:
+    // instead of active could be modeLoop: true/false.
+    // If true, will loop on the last pressed note, if false play only once.
+    // There should also be the possibility to set last note to null...
     bool active = false;
 
-    IO_AudioLoop(AudioCore* _core) { core = _core; }
+    byte currentPattern = 0;
+    Pattern *pattern;
 
-    void next() {
+    IO_AudioLoop(AudioCore *_core) { core = _core; }
+
+    void init() { setCurrentPattern(0); }
+
+    void setCurrentPattern(byte value)
+    {
+        currentPattern = value;
+        pattern = &patterns[currentPattern];
+    }
+
+    void next()
+    {
         // if (lastStep.duration != 255) {
         //     lastStep.duration--;
         //     if (!lastStep.slide) {
---
+++ io_arp_patterns.h
@@ -0,0 +1,11 @@
+#ifndef IO_ARP_PATTERNS_H_
+#define IO_ARP_PATTERNS_H_
+
+#include "Pattern.h"
+
+#define PATTERN_COUNT 127
+Pattern patterns[PATTERN_COUNT];
+
+// need to create list of patterns
+
+#endif
