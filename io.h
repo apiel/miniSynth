5941920b98fa0871e6cc36023b0a8f10c1d9ad79
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-03-18 22:18:21 +0100

    fix

--- io.h
+++ io.h
@@ -13,16 +13,15 @@
 #include "controller/io_controller_akai_mpk_mini.h"
 
 IO_AudioSynth synths[SYNTH_COUNT];
+IO_AudioSynth *synthsPtr[SYNTH_COUNT] = {&synths[SYNTH_0], &synths[SYNTH_1], &synths[SYNTH_2], &synths[SYNTH_3]};
+
 IO_AudioLoop loop0(&synths[SYNTH_0], SYNTH_0);
 IO_AudioLoop loop1(&synths[SYNTH_1], SYNTH_1);
 IO_AudioLoop loop2(&synths[SYNTH_2], SYNTH_2);
 IO_AudioLoop loop3(&synths[SYNTH_3], SYNTH_3);
-// IO_AudioLoop **loops = ;
+IO_AudioLoop *loopsPtr[SYNTH_COUNT] = {&loop0, &loop1, &loop2, &loop3};
 
-IO_ControllerAkaiMPKmini controller(
-    &display,
-    {&loop0, &loop1, &loop2, &loop3},
-    {&synths[SYNTH_0], &synths[SYNTH_1], &synths[SYNTH_2], &synths[SYNTH_3]});
+IO_ControllerAkaiMPKmini controller(&display, loopsPtr, synthsPtr);
 
 AudioOutputMQS audioOut;
 AudioMixer4 mixerOutput;
