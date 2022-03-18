f07f7090623f33ae0a9215952d1a6156290c4fd1
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-03-18 22:19:53 +0100

    cleanup

--- io_midi.h
+++ io_midi.h
@@ -2,9 +2,17 @@
 #define IO_MIDI_H_
 
 #include <Arduino.h>
+#include <USBHost_t36.h>
 
 #include "io_display.h"
-#include "io_midi_core.h"
+
+#define MIDI_COUNT 4
+
+USBHost myusb;
+USBHub hub1(myusb);
+USBHub hub2(myusb);
+USBHub hub3(myusb);
+MIDIDevice midi[MIDI_COUNT] = MIDIDevice(myusb);
 
 void noteOnHandler(byte channel, byte note, byte velocity)
 {
