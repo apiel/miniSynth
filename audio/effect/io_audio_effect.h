5358968238687cb721ccadc43b356ede3f44324b
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-03-21 20:39:59 +0100

    reverb filter

--- audio/effect/io_audio_effect.h
+++ audio/effect/io_audio_effect.h
@@ -11,33 +11,87 @@ enum
 {
     IFX_OFF,
     IFX_DIST,
+    IFX_REVERB,
     IFX_COUNT
 };
-
-// to solve https://forum.pjrc.com/threads/58566-Changing-effects-at-run-time
-// or https://forum.pjrc.com/threads/64446-Dynamic-Audio-Connection-Bug-A-new-one
 
 class IO_AudioEffect
 {
 protected:
     AudioDumb dumb;
     AudioEffectDistortion dist;
+    AudioEffectFreeverb reverb;
 
 public:
     AudioDumb input;
     AudioDumb output;
 
     AudioConnection patches[IFX_COUNT][2] = {
-        {AudioConnection(input, dumb), AudioConnection(dumb, output)}, // OFF
-        {AudioConnection(input, dist), AudioConnection(dist, output)}  // IFX_DIST
+        {AudioConnection(input, dumb), AudioConnection(dumb, output)},    // OFF
+        {AudioConnection(input, dist), AudioConnection(dist, output)},    // IFX_DIST
+        {AudioConnection(input, reverb), AudioConnection(reverb, output)} // IFX_REVERB
     };
 
-    byte currentEffect = IFX_DIST;
-    // byte currentEffect = IFX_OFF;
+    byte currentEffect = IFX_OFF;
+    byte edit1Value = 0;
+    byte edit2Value = 0;
 
     IO_AudioEffect(void)
     {
         setEffect(currentEffect);
+    }
+
+    void edit1(byte value)
+    {
+        edit1Value = value;
+
+        float pctVal = value / 127.0f;
+        switch (currentEffect)
+        {
+        case IFX_DIST:
+            // 1000.0f I am not sure why, maybe it can be more?
+            dist.distortion(pctVal * pctVal * 1000.0f);
+            break;
+        case IFX_REVERB:
+            reverb.roomsize(pctVal);
+            break;
+        default:
+            break;
+        }
+    }
+
+    void edit2(byte value)
+    {
+        edit2Value = value;
+
+        float pctVal = value / 127.0f;
+        switch (currentEffect)
+        {
+        case IFX_DIST:
+            dist.setRange((pctVal * pctVal * 999.0f) + 1);
+            break;
+        case IFX_REVERB:
+            reverb.damping(pctVal);
+            break;
+        default:
+            break;
+        }
+    }
+
+    const char *getName()
+    {
+
+        switch (currentEffect)
+        {
+        case IFX_OFF:
+            return "Off";
+        case IFX_DIST:
+            return "Distortion";
+        case IFX_REVERB:
+            return "Reverb";
+        default:
+            return "unknown yet";
+        }
     }
 
     void setEffect(byte value)
@@ -57,40 +111,8 @@ public:
                 patches[i][1].disconnect();
             }
         }
-    }
-
-    void edit1(byte value)
-    {
-        float pctVal = value / 127.0f;
-        if (currentEffect == IFX_DIST)
-        {
-            // 1000.0f i am not sure why, maybe it can be more?
-            dist.distortion(pctVal * pctVal * 1000.0f);
-            // Serial.printf("Edit distor %.6f\n", pctVal * pctVal * 1000.0f);
-        }
-    }
-
-    void edit2(byte value)
-    {
-        float pctVal = value / 127.0f;
-        if (currentEffect == IFX_DIST)
-        {
-            dist.setRange((pctVal * pctVal * 999.0f) + 1);
-        }
-    }
-
-    const char *getName()
-    {
-
-        switch (currentEffect)
-        {
-        case IFX_OFF:
-            return "Off";
-        case IFX_DIST:
-            return "Distortion";
-        default:
-            return "unknown yet";
-        }
+        edit1(edit1Value);
+        edit2(edit2Value);
     }
 };
 
